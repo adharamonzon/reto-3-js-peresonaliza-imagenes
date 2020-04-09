8f38be4e31e4f3aac0fc9c83aa9da96a13c1f559
adharamonzon <adharamonzon@gmail.com>
2020-04-09 18:51:11 +0200

    :zap: add functionality: add your own image

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ function handleUpdate() {
 
 inputs.forEach((input) => input.addEventListener('change', handleUpdate));
 inputs.forEach((input) => input.addEventListener('mouseover', handleUpdate));
+
+//seleccionar tu propia foto
+
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
+const fr = new FileReader();
+
+const getImage = (ev) => {
+  debugger;
+  var myFile = ev.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
+};
+
+function writeImage() {
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  profilePreview.style.backgroundImage = `url(${fr.result})`;
+}
+
+fileField.addEventListener('change', getImage);
